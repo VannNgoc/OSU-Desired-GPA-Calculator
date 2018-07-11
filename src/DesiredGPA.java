8f30fedcf58a8ee11653ab3fea9c423d57284cda
Vannaroth Ngoc <vannaroth.ngoc@duplexlabs.com>
2018-07-11 00:13:37 -0400

    Fixed the trash code.

--- src/DesiredGPA.java
+++ src/DesiredGPA.java
@@ -6,9 +6,6 @@ public class DesiredGPA
 		DesiredGPAView view = new DesiredGPAView();
 		DesiredGPAModel model = new DesiredGPAModel();
 		DesiredGPAController controller = new DesiredGPAController(view, model);
-
-		//Remove unneeded line
-		int i = 0;
 		
 		view.registerObserver(controller);
 	}
